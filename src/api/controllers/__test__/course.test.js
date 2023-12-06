b640e2cf211b0cc819b83b9e556ca4afc710f232
Hansvirgo <hansvirgoding@gmail.com>
2023-12-06 12:03:18 +0700

    Add test jest course controller and services

---
+++ src/api/controllers/__test__/course.test.js
@@ -0,0 +1,472 @@
+import { jest } from '@jest/globals';
+import { ApplicationError } from '../../../libs/error.js';
+
+/** @typedef {Record<keyof import('../../services/course.js'), jest.Mock>} CourseControllerMock */
+/** @typedef {Record<keyof import('../../services/course.js'), jest.Mock>} CourseServiceMock */
+
+jest.unstable_mockModule(
+  '../../services/course.js',
+  () =>
+    /** @type {CourseServiceMock} */
+    ({
+      getCourses: jest.fn(),
+      getCourseById: jest.fn(),
+      createCourse: jest.fn(),
+      updateCourse: jest.fn(),
+      destroyCourse: jest.fn()
+    })
+);
+
+const courseController = /** @type {CourseControllerMock} */ (
+  await import('../course.js')
+);
+
+const courseService = /** @type {CourseServiceMock} */ (
+  await import('../../services/course.js')
+);
+
+describe('Course controller', () => {
+  describe('Get courses', () => {
+    it('returns list of courses', async () => {
+      const mockCourses = [
+        {
+          id: '1',
+          name: 'Emilia'
+        },
+        {
+          id: '2',
+          name: 'Rem'
+        },
+        {
+          id: '3',
+          name: 'Ram'
+        }
+      ];
+
+      courseService.getCourses.mockResolvedValue(
+        // @ts-ignore
+        mockCourses
+      );
+
+      const mockRequest = {};
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.getCourses(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({ data: mockCourses });
+    });
+
+    it('throws application error when getCourses fails', async () => {
+      const mockError = new ApplicationError('Failed to get course', 500);
+
+      courseService.getCourses.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {};
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.getCourses(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when getCourses fails', async () => {
+      const mockError = new Error();
+
+      courseService.getCourses.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {};
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.getCourses(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+
+  describe('Get course by id', () => {
+    it('get a course', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'Emilia'
+      };
+
+      courseService.getCourseById.mockResolvedValue(
+        // @ts-ignore
+        mockCourse
+      );
+
+      const mockRequest = {
+        params: { id: mockCourse.id }
+      };
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.getCourseById(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        data: mockCourse
+      });
+    });
+
+    it('throws application error when getCourseById fails', async () => {
+      const mockError = new ApplicationError('Failed to get course', 500);
+
+      courseService.getCourseById.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.getCourseById(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when getCourseById fails', async () => {
+      const mockError = new Error();
+
+      courseService.getCourseById.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.getCourseById(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+
+  describe('Create course', () => {
+    it('creates a course', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'course1'
+      };
+
+      const image = 'default image update later';
+
+      const mockCourseWithImage = { ...mockCourse, image: image };
+
+      courseService.createCourse.mockResolvedValue(
+        // @ts-ignore
+        mockCourse
+      );
+
+      const mockRequest = {
+        body: mockCourseWithImage
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.createCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(201);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Course created successfully',
+        data: mockCourse
+      });
+    });
+
+    it('throws application error when createCourse fails', async () => {
+      const mockError = new ApplicationError('Failed to create course', 500);
+
+      courseService.createCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {};
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.createCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when createCourse fails', async () => {
+      const mockError = new Error();
+
+      courseService.createCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {};
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.createCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+
+  describe('Update course', () => {
+    it('updates a course', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'Emilia',
+        image: 'default'
+      };
+
+      const oldCourseData = {
+        id: '1',
+        name: 'Old Course Name',
+        image: 'oldImage.jpg'
+      };
+
+      // @ts-ignore
+      courseService.getCourseById.mockResolvedValue(oldCourseData);
+
+      courseService.updateCourse.mockResolvedValue(
+        // @ts-ignore
+        mockCourse
+      );
+
+      const mockRequest = {
+        body: mockCourse,
+        params: { id: mockCourse.id }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.updateCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Course updated successfully',
+        data: mockCourse
+      });
+    });
+
+    it('throws application error when updateCourse fails', async () => {
+      const mockError = new ApplicationError('Failed to update course', 500);
+
+      courseService.updateCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.updateCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when updateCourse fails', async () => {
+      const mockError = new Error();
+
+      courseService.updateCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.updateCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+
+  describe('Destroy course', () => {
+    it('destroys a course', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'Toyota'
+      };
+
+      courseService.destroyCourse.mockResolvedValue(
+        // @ts-ignore
+        mockCourse
+      );
+
+      const mockRequest = {
+        params: { id: mockCourse.id }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      // @ts-ignore
+      await courseController.destroyCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(204);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Course deleted successfully',
+        data: null
+      });
+    });
+
+    it('throws application error when destroyCourse fails', async () => {
+      const mockError = new ApplicationError('Failed to destroy course', 500);
+
+      courseService.destroyCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.destroyCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when destroyCourse fails', async () => {
+      const mockError = new Error();
+
+      courseService.destroyCourse.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: 1
+        }
+      };
+
+      const mockResponse = {
+        locals: { user: {} },
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+      };
+
+      await courseController.destroyCourse(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+});
---
+++ src/api/services/__test__/course.test.js
@@ -0,0 +1,201 @@
+import { jest } from '@jest/globals';
+import { ApplicationError } from '../../../libs/error.js';
+
+/** @typedef {Record<keyof import('../../repositories/course.js'), jest.Mock>} CourseRepositoryMock */
+/** @typedef {Record<keyof import('../course.js'), jest.Mock>} CourseServiceMock */
+
+jest.unstable_mockModule(
+  '../../repositories/course.js',
+  () =>
+    /** @type {CourseRepositoryMock} */
+    ({
+      getCourses: jest.fn(),
+      getCourseById: jest.fn(),
+      createCourse: jest.fn(),
+      updateCourse: jest.fn(),
+      destroyCourse: jest.fn()
+    })
+);
+
+const courseRepository = /** @type {CourseRepositoryMock} */ (
+  await import('../../repositories/course.js')
+);
+
+const courseService = /** @type {CourseServiceMock} */ (
+  await import('../course.js')
+);
+
+describe('Courses service', () => {
+  describe('Get Courses', () => {
+    it('returns course data', async () => {
+      const mockCourses = [
+        {
+          id: '1',
+          name: 'Emilia'
+        }
+      ];
+
+      courseRepository.getCourses.mockResolvedValue(
+        /** @ts-ignore */
+        mockCourses
+      );
+
+      const courses = await courseService.getCourses({});
+
+      expect(courses).toEqual(mockCourses);
+    });
+
+    it('throws application error when getting courses fails', async () => {
+      const mockError = new ApplicationError('Failed to get courses', 500);
+
+      courseRepository.getCourses.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.getCourses({})).rejects.toThrow(
+        `Error while getting courses: ${mockError.message}`
+      );
+    });
+  });
+
+  describe('Get course', () => {
+    it('returns course data', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'Emilia'
+      };
+
+      courseRepository.getCourseById.mockResolvedValue(
+        /** @ts-ignore */
+        mockCourse
+      );
+
+      const course = await courseService.getCourseById('1');
+
+      expect(course).toEqual(mockCourse);
+    });
+
+    it('throws application error when getting course details fails', async () => {
+      const mockError = new ApplicationError('Failed to get course', 500);
+
+      courseRepository.getCourseById.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.getCourseById('1')).rejects.toThrow(
+        `Error while getting course details: ${mockError.message}`
+      );
+    });
+
+    it('throws application error when course is not found', async () => {
+      courseRepository.getCourseById.mockResolvedValue(
+        /** @ts-ignore */
+        null
+      );
+
+      await expect(courseService.getCourseById('1')).rejects.toThrow(
+        'Error while getting course details: Course not found'
+      );
+    });
+
+    it('throws application error when getting course fails', async () => {
+      const mockError = new ApplicationError('Failed to get course', 500);
+
+      courseRepository.getCourseById.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.getCourseById('1')).rejects.toThrow(
+        `Error while getting course details: ${mockError.message}`
+      );
+    });
+  });
+
+  describe('Create course', () => {
+    it('returns created course data with the user id that creates', async () => {
+      const userId = '1';
+
+      const mockCourse = {
+        id: '1',
+        name: 'Emilia'
+      };
+
+      const mockCourseWithUserId = {
+        ...mockCourse,
+        userId
+      };
+
+      courseRepository.createCourse.mockResolvedValue(
+        /** @ts-ignore */
+        mockCourseWithUserId
+      );
+
+      const course = await courseService.createCourse(mockCourse, userId);
+
+      expect(course).not.toEqual(mockCourse);
+      expect(course).toEqual(mockCourseWithUserId);
+    });
+
+    it('throws application error when creating course fails', async () => {
+      const mockError = new ApplicationError('Failed to create course', 500);
+
+      courseRepository.createCourse.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.createCourse({}, '1')).rejects.toThrow(
+        `Error while creating course: ${mockError.message}`
+      );
+    });
+  });
+
+  describe('Update course', () => {
+    it('returns updated course data with that user id updates it', async () => {
+      const mockCourse = {
+        id: '1',
+        name: 'Emilia'
+      };
+
+      courseRepository.updateCourse.mockResolvedValue(
+        /** @ts-ignore */
+        [null, [mockCourse]]
+      );
+
+      const course = await courseService.updateCourse('1', mockCourse);
+
+      expect(course).toEqual(mockCourse);
+    });
+
+    it('throws application error when updating course fails', async () => {
+      const mockError = new ApplicationError('Failed to update course', 500);
+
+      courseRepository.updateCourse.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.updateCourse('1', {}, '1')).rejects.toThrow(
+        `Error while updating course: ${mockError.message}`
+      );
+    });
+  });
+
+  describe('Destroy course', () => {
+    it('throws application error when destroying course fails', async () => {
+      const mockError = new ApplicationError('Failed to destroy course', 500);
+
+      courseRepository.destroyCourse.mockRejectedValue(
+        /** @ts-ignore */
+        mockError
+      );
+
+      await expect(courseService.destroyCourse('1')).rejects.toThrow(
+        `Error while deleting course: ${mockError.message}`
+      );
+    });
+  });
+});
