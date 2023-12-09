8bc3e0e66f744bcecf213d305ac39abda46e9fa2
Hansvirgo <hansvirgoding@gmail.com>
2023-12-10 00:20:07 +0700

    change services user-payment unmanaged to managed transaction

--- src/api/services/user-payment.js
+++ src/api/services/user-payment.js
@@ -24,69 +24,71 @@ export async function payCourse(courseId, userId) {
 
 /** @param {{ payment_method?: any; course_id?: any }} params */
 export async function updatePayCourse(paymentMethod, paymentId, userId) {
-  const t = await sequelize.transaction();
   try {
-    // GET COURSE ID
-    const courseId = await paymentRepository.getCourseIdByPaymentId(paymentId, {
-      transaction: t
-    });
-
-    // USER COURSE PAYLOAD
-    const userCoursePayload = {
-      user_id: userId,
-      course_id: courseId
-    };
-
-    // CHECK AND CREATE USER COURSE
-    // Check if userCourse already exists
-    const existingUserCourse =
-      await userCourseRepository.getUserCourseByUserIdAndCourseId(
-        userId,
-        courseId,
-        { transaction: t }
-      );
-    if (existingUserCourse) {
-      throw new Error(
-        'User is already enrolled in this course. Transaction rolled back.'
-      );
-    } else {
-      await userCourseRepository.createUserCourse(userCoursePayload, {
-        transaction: t
-      });
-    }
-
-    // GET ALL MATERIALS ID
-    const materialsId =
-      await courseMaterialRepository.getCourseMaterialByCourseId(courseId, {
-        transaction: t
-      });
-
-    // CREATE MATERIAL STATUS
-    for (const course_material_id of materialsId) {
-      await courseMaterialStatusRepository.setCourseMaterialStatus(
+    await sequelize.transaction(async (t) => {
+      // GET COURSE ID
+      const courseId = await paymentRepository.getCourseIdByPaymentId(
+        paymentId,
         {
-          user_id: userId,
-          course_material_id
-        },
+          transaction: t
+        }
+      );
+
+      // USER COURSE PAYLOAD
+      const userCoursePayload = {
+        user_id: userId,
+        course_id: courseId
+      };
+
+      // CHECK AND CREATE USER COURSE
+      // Check if userCourse already exists
+      const existingUserCourse =
+        await userCourseRepository.getUserCourseByUserIdAndCourseId(
+          userId,
+          courseId,
+          { transaction: t }
+        );
+      if (existingUserCourse) {
+        throw new Error(
+          'User is already enrolled in this course. Transaction rolled back.'
+        );
+      } else {
+        await userCourseRepository.createUserCourse(userCoursePayload, {
+          transaction: t
+        });
+      }
+
+      // GET ALL MATERIALS ID
+      const materialsId =
+        await courseMaterialRepository.getCourseMaterialByCourseId(courseId, {
+          transaction: t
+        });
+
+      // CREATE MATERIAL STATUS
+      for (const course_material_id of materialsId) {
+        await courseMaterialStatusRepository.setCourseMaterialStatus(
+          {
+            user_id: userId,
+            course_material_id
+          },
+          { transaction: t }
+        );
+      }
+
+      const payload = {
+        payment_status: 'COMPLETED',
+        payment_method: paymentMethod
+      };
+
+      const [, [updatePay]] = await paymentRepository.updatePayCourse(
+        payload,
+        paymentId,
         { transaction: t }
       );
-    }
 
-    const payload = {
-      payment_status: 'COMPLETED',
-      payment_method: paymentMethod
-    };
-
-    const [, [updatePay]] = await paymentRepository.updatePayCourse(
-      payload,
-      paymentId,
-      { transaction: t }
-    );
-
-    await t.commit();
-    return updatePay;
+      return updatePay;
+    });
   } catch (error) {
-    await t.rollback();
     throw generateApplicationError(error, 'Payment is error', 500);
   }
 }
