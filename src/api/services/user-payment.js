5befb26e5528f69b81126c35f4c393940bd3bd67
Hansvirgo <hansvirgoding@gmail.com>
2023-12-13 19:52:59 +0700

    fix status code on payment method null

--- src/api/services/user-payment.js
+++ src/api/services/user-payment.js
@@ -102,7 +102,7 @@ export async function updatePayCourse(
 ) {
   try {
     if (!paymentMethod) {
-      throw new ApplicationError('Payment method cannot be null', 422);
+      throw new ApplicationError('Payment method cannot be null', 400);
     }
 
     // CHECK STATUS PAYMENT
