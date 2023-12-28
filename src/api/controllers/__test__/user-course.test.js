25e907ac4390d043929e49d426a15ece88ef70b2
irfants22 <irfanee02@gmail.com>
2023-12-28 17:54:02 +0700

    create unit test for user course

---
+++ src/api/controllers/__test__/user-course.test.js
@@ -0,0 +1,118 @@
+import { jest } from '@jest/globals';
+import { ApplicationError } from '../../../libs/error.js';
+
+/** @typedef {Record<keyof import('../../services/user-course.js'), jest.Mock>} UserCourseControllerMock */
+/** @typedef {Record<keyof import('../../services/user-course.js'), jest.Mock>} UserCourseServiceMock */
+
+jest.unstable_mockModule(
+  '../../services/user-course.js',
+  () =>
+    /** @type {UserCourseServiceMock} */
+    ({
+      setOnboardedTrue: jest.fn()
+    })
+);
+
+const userCourseController = /** @type {UserCourseControllerMock} */ (
+  await import('../user-course.js')
+);
+
+const userCourseService = /** @type {UserCourseServiceMock} */ (
+  await import('../../services/user-course.js')
+);
+
+describe('User course controller', () => {
+  describe('Set onboarderd to true', () => {
+    it('update onboarded status on user course', async () => {
+      const mockUserCourses = {
+        id: '123',
+        user_id: '123',
+        onboarded: true
+      };
+
+      userCourseService.setOnboardedTrue.mockResolvedValue(
+        // @ts-ignore
+        mockUserCourses
+      );
+
+      const mockRequest = {
+        params: {
+          id: '1'
+        }
+      };
+
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      // @ts-ignore
+      await userCourseController.setOnboardedTrue(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Onboarded updated successfully',
+        data: mockUserCourses
+      });
+    });
+
+    it('throws application error when setOnboardedTrue fails', async () => {
+      const mockError = new ApplicationError(
+        'Failed to update user courses',
+        500
+      );
+
+      userCourseService.setOnboardedTrue.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: '1'
+        }
+      };
+
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      // @ts-ignore
+      await userCourseController.setOnboardedTrue(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(mockError.statusCode);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: mockError.message
+      });
+    });
+
+    it('throws generic error when setOnboardedTrue fails', async () => {
+      const mockError = new Error();
+
+      userCourseService.setOnboardedTrue.mockRejectedValue(
+        // @ts-ignore
+        mockError
+      );
+
+      const mockRequest = {
+        params: {
+          id: '1'
+        }
+      };
+
+      const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      // @ts-ignore
+      await userCourseController.setOnboardedTrue(mockRequest, mockResponse);
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Internal server error'
+      });
+    });
+  });
+});
---
+++ src/api/services/__test__/user-course.test.js
@@ -0,0 +1,71 @@
+import { jest } from '@jest/globals';
+import { ApplicationError } from '../../../libs/error.js';
+
+/**
+ * @typedef {Record<
+ *   keyof import('../../repositories/user-course.js'),
+ *   jest.Mock
+ * >} UserCourseRepositoryMock
+ */
+/** @typedef {Record<keyof import('../user-course.js'), jest.Mock>} UserCourseServiceMock */
+
+jest.unstable_mockModule(
+  '../../repositories/user-course.js',
+  () =>
+    /** @type {UserCourseRepositoryMock} */
+    ({
+      setOnboardedTrue: jest.fn()
+    })
+);
+
+const userCourseRepository = /** @type {UserCourseRepositoryMock} */ (
+  await import('../../repositories/user-course.js')
+);
+
+const userCourseService = /** @type {UserCourseServiceMock} */ (
+  await import('../user-course.js')
+);
+
+describe('User course service', () => {
+  test('should update onboarded status and return the updated user course', async () => {
+    const userId = '123';
+    const mockUserCourse = { id: '123', onboarded: true };
+
+    // @ts-ignore
+    userCourseRepository.setOnboardedTrue.mockResolvedValue([
+      1,
+      [mockUserCourse]
+    ]);
+
+    const updatedUserCourse = await userCourseService.setOnboardedTrue(userId);
+
+    expect(updatedUserCourse).toEqual(mockUserCourse);
+    expect(userCourseRepository.setOnboardedTrue).toHaveBeenCalledWith(userId);
+  });
+
+  test('should throw an ApplicationError when user course is not found', async () => {
+    const userId = '123';
+
+    // @ts-ignore
+    userCourseRepository.setOnboardedTrue.mockResolvedValue([1, []]);
+
+    await expect(userCourseService.setOnboardedTrue(userId)).rejects.toThrow(
+      'User Course Not Found'
+    );
+  });
+
+  test('should throw a generated ApplicationError when an unexpected error occurs', async () => {
+    const userId = '123';
+    const mockError = new ApplicationError(
+      'Failed to update user courses',
+      500
+    );
+
+    // @ts-ignore
+    userCourseRepository.setOnboardedTrue.mockRejectedValue(mockError);
+
+    await expect(userCourseService.setOnboardedTrue(userId)).rejects.toThrow(
+      'Error while updating onboarded status'
+    );
+  });
+});
